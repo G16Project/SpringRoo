dd7c926b5270a3798196400f004e3f844be34beb
Pol Chaipetch <dekwidwa.nr@gmail.com>
2012-02-16 22:53:53 +0700

    Enum Recue

---
+++ src/test/java/project/group16/entity/FinanceDepertmentIntegrationTest.java
@@ -0,0 +1,12 @@
+package project.group16.entity;
+
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = FinanceDepertment.class)
+public class FinanceDepertmentIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
