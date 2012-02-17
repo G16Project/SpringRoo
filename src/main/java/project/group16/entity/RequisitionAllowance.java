2b2739bf13cbf9dc1ad1f59b477af5926b23b9c0
Kie <inmylife_itme@hotmail.com>
2012-02-16 21:54:35 -0800

    Kie

---
+++ src/main/java/project/group16/entity/RequisitionAllowance.java
@@ -0,0 +1,19 @@
+package project.group16.entity;
+
+import java.util.Date;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class RequisitionAllowance {
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date dateWork;
+}
---
+++ src/test/java/project/group16/entity/RequisitionAllowanceIntegrationTest.java
@@ -0,0 +1,12 @@
+package project.group16.entity;
+
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = RequisitionAllowance.class)
+public class RequisitionAllowanceIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
